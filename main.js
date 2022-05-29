5a995fdcb90dc5d596bbc11f41b4c6d366fc1a3c
Emma Remon <90062286+ecs3edec3ex232@users.noreply.github.com>
2022-05-29 14:33:10 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function Preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function Take_Snapshot(){
+    save('photo_with_lipstick.jpeg');
+}
